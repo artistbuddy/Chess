aa8e6d3b2bc76a4f8a37b7958aeb498f5e9c5dfa
artistbuddy <artist.buddy@gmail.com>
2017-02-11 17:40:22 +0100

    Initial commit

---
+++ Chess/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Chess
+//
+//  Created by Karol on 11/02/2017.
+//  Copyright © 2017 Karol Bukowski. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
